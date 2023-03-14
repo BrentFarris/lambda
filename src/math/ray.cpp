a2b81e8d70ecedb81605a1d57d96ecb0dd192f73
Brent Farris <brentfarris@outlook.com>
2023-03-14 18:12:57 -0500

    Fixing self error

--- src/math/ray.cpp
+++ src/math/ray.cpp
@@ -4,6 +4,6 @@
 bool Ray::triangle_hit(double rayLen, const Vector3& a,
 	const Vector3& b, const Vector3& c) const
 {
-	LineSegment s = { origin, r.point(rayLen) };
+	LineSegment s = { origin, point(rayLen) };
 	return s.triangle_hit(a, b, c);
 }
