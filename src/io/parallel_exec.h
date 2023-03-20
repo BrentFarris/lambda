c51374f6c70c5cd80e472df95ef9f5ceadd7a499
Brent Farris <brentfarris@outlook.com>
2023-03-19 21:23:46 -0500

    Fixing bad vector ctor arg

--- src/io/parallel_exec.h
+++ src/io/parallel_exec.h
@@ -14,15 +14,15 @@ namespace lambda {
 	static void parallel_exec(T* self, void(*call)(const ParallelExecState<T>*), size_t collectionSize) {
 		size_t threadCount = std::thread::hardware_concurrency();
 		int stagger = (int)std::min(threadCount, collectionSize);
-		std::vector<ParallelExecState<T>> parallels(threadCount);
+		std::vector<ParallelExecState<T>> parallels{};
+		parallels.reserve(stagger);
 		for (int i = 0; i < stagger; ++i) {
 			parallels.push_back({});
 			ParallelExecState<T>& state = parallels.back();
 			state.self = self;
 			state.start = i;
 			state.stagger = stagger;
-			parallels[parallels.size() - 1].thread =
-				std::thread(call, &parallels[parallels.size() - 1]);
+			state.thread = std::thread(call, &parallels[parallels.size() - 1]);
 		}
 		for (auto& p : parallels)
 			if (p.thread->joinable())
